b619cdae6bcaef603e64efc84507721691bfbddd
Roman P <git@romanempire.dev>
2026-03-29 08:26:27 +0100

    feat: - Thumbnail Virtual Threads

--- src/main/java/dev/romanempire/framd/thumbnails/ThumbnailService.java
+++ src/main/java/dev/romanempire/framd/thumbnails/ThumbnailService.java
@@ -9,19 +9,28 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 
 @Service
 public class ThumbnailService {
+
 
     @Value("${thumbnails.path}")
     private String thumbnailPath;
 
     private static final Logger logger = LoggerFactory.getLogger(ThumbnailService.class);
 
-    public Map<String, String> generateThumbnails(Map<String, String> hashes) {
-        Map<String, String> hashToPath = new HashMap<>();
+
+    ///
+    /// Generates Thumbnails for each Path passed
+    ///
+    /// @param hashesByPath Map of String path to String Hash
+    /// @return Map of String hash to generated thumbnail path
+    public Map<String, String> generateThumbnails(Map<String, String> hashesByPath) {
+        Map<String, String> thumbnailPathsByHash = new ConcurrentHashMap<>();
 
         logger.info("Generating Thumbnails");
 
@@ -29,7 +38,8 @@ public class ThumbnailService {
         try {
             Files.createDirectories(thumbsDir); // no-op if already exists
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            logger.error("Failed to make Thumbnail Directory {} with error: {}", thumbnailPath, e.getMessage());
+            return thumbnailPathsByHash;
         }
 
         hashes.forEach((path, hash) -> {
@@ -49,5 +59,38 @@ public class ThumbnailService {
             }
         });
         return hashToPath;
+        var semaphore = new Semaphore(20);
+
+
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            hashesByPath.forEach((path, hash) ->
+                    executor.submit(() -> {
+                        try {
+                            semaphore.acquire();
+                            var outputPath = generateThumbnail(path);
+                            thumbnailPathsByHash.put(hash, outputPath.toString());
+                        } catch (IOException e) {
+                            logger.error("Failed generating hash: {} with error {}", hash, e.getMessage());
+                        } catch (InterruptedException e) {
+                            logger.error("Semaphore acquire got interrupted: {}", e.getMessage());
+                        } finally {
+                            semaphore.release();
+                        }
+                    }));
+        }
+        logger.info("Generated {}/{} files", thumbnailPathsByHash.size(), hashesByPath.size());
+        return thumbnailPathsByHash;
+    }
+
+    private Path generateThumbnail(String path) throws IOException {
+        var sourcePath = Path.of(path);
+        var outputDir = Path.of(thumbnailPath).resolve(sourcePath.getRoot().relativize(sourcePath.getParent()));
+        var outputPath = outputDir.resolve(sourcePath.getFileName());
+        Files.createDirectories(outputDir);
+        Thumbnails.of(sourcePath.toFile())
+                .scale(0.3)
+                .outputQuality(0.7)
+                .toFile(outputPath.toFile());
+        return outputPath;
     }
 }
