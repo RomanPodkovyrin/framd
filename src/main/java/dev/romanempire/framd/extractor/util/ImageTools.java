6bb8bbf7b3b00be29224ca492f725ce0c139bf28
Roman P <git@romanempire.dev>
2026-04-05 16:24:44 +0100

    feat: test and fix ImageTools utils

--- src/main/java/dev/romanempire/framd/extractor/util/ImageTools.java
+++ src/main/java/dev/romanempire/framd/extractor/util/ImageTools.java
@@ -18,28 +18,46 @@ public class ImageTools {
     public record FileNameParts(String name, String extension) {
     }
 
-    private static final Set<String> IMAGE_EXTENSIONS = Set.of(
-            "jpg", "jpeg", "png", "webp", "gif", "bmp", "wbmp"
-    );
+    private static final Set<String> IMAGE_EXTENSIONS = Set.of("jpg", "jpeg", "png", "webp", "gif", "bmp", "wbmp");
+
+
+    private static final Set<String> ILLEGAL_EXTENSIONS = Set.of("", "/", ".", "..");
 
     public static Boolean isImage(Path path) {
-        FileNameParts fileNameParts = getFileParts(path);
+        FileNameParts fileNameParts;
+        try {
+            fileNameParts = getFileParts(path);
+        } catch (IllegalArgumentException ex) {
+            logger.error(ex.getMessage());
+            return false;
+        }
+
+        System.out.println(fileNameParts.extension);
 
 
         return IMAGE_EXTENSIONS.contains(fileNameParts.extension().toLowerCase());
     }
 
-    public static FileNameParts getFileParts(Path path) {
+    public static FileNameParts getFileParts(Path path) throws IllegalArgumentException {
+        if (ILLEGAL_EXTENSIONS.contains(path.toString())) {
+            throw new IllegalArgumentException("Invalid file name");
+        }
         var fileName = path.getFileName().toString();
 
         var dotIndex = fileName.lastIndexOf(".");
         if (dotIndex == -1) {
             // no extension
             return new FileNameParts(fileName, "");
+        } else if (dotIndex == 0) { // hidden file without extension
+            // There are no other "." in the string
+            // safe to assume no extension
+            return new FileNameParts(fileName.substring(1), "");
         }
+
+
         var name = fileName.substring(0, dotIndex);
         var extension = fileName.substring(dotIndex + 1);
-        return new FileNameParts(name, extension);
+        return new FileNameParts(name.charAt(0) == '.' ? name.substring(1) : name, extension);
     }
 
 
---
+++ src/test/java/dev/romanempire/framd/extractor/util/ImageToolsTest.java
@@ -0,0 +1,60 @@
+package dev.romanempire.framd.extractor.util;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageToolsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/root/file.png", "photo.jpg", "./pic.webp",})
+    void isImage(String path) {
+        assertTrue(ImageTools.isImage(Path.of(path)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/root/file.txt", "photo", "/.jpfeg", "./", "", "1223/file.heic", "/.jpeg", "/.jPEg",})
+    void isNotImage(String path) {
+        assertFalse(ImageTools.isImage(Path.of(path)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"/root/file.txt, file, txt", // normal file
+            ".file, file, ''", // hidden file without extension
+            "file, file, ''", // file without extension
+            ".file.txt, file, txt", // hidden file with extension
+            "file., file, ''", // trailing dot
+            ".file., file, ''", // hidden file with trailing dot
+            "file.tar.gz, file.tar, gz", // multiple extensions
+            ".file.tar.gz, file.tar, gz" // hidden file with multiple extensions
+    })
+    void testGetFileParts(String path, String expectedFileName, String expectedFileExtension) {
+
+        var fileParts = ImageTools.getFileParts(Path.of(path));
+
+        assertEquals(expectedFileName, fileParts.name());
+        assertEquals(expectedFileExtension, fileParts.extension());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/",
+            ".",
+            "..",
+            "",
+    })
+    void testGetFilePartsMalformed(String input) {
+
+        Exception ex = assertThrows(
+                Exception.class,
+                () -> ImageTools.getFileParts(Path.of(input))
+        );
+
+        assertEquals("Invalid file name", ex.getMessage());
+
+    }
+
+}
